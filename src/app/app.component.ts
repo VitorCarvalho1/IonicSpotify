657ed152b3131598fc8839413910a2688e0725b1
VitorCarvalho1 <vitorhugo.carvalho2@yahoo.com.br>
2022-09-12 22:47:01 -0300

    add app-routes-ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { Platform } from '@ionic/angular'
+import { Platform } from '@ionic/angular';
 
 
 
--- src/app/splash/splash.page.ts
+++ src/app/splash/splash.page.ts
@@ -13,7 +13,7 @@ export class SplashPage implements OnInit {
 
   ngOnInit() {
      setTimeout(() => {
-       this.router.navigateByUrl('home')
-    }, 2000)
+       this.router.navigateByUrl('home');
+    }, 2000);
   }
 }
